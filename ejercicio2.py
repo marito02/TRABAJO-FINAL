6bb129c7d2625d653f11d66fa6defa4616a72990
marito02 <mariolavanderaa@gmail.com>
2022-12-11 20:21:08 +0100

    ej2bueno

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,77 +1,78 @@
 from __future__ import annotations
 
-solution = []
+solucion = []
 
 
-def is_safe(board: list[list[int]], row: int, column: int) -> bool:
+def is_safe(tablero: list[list[int]], fila: int, columna: int) -> bool:
     """
-    This function returns a boolean value True if it is safe to place a queen there
-    considering the current state of the board.
-    Parameters :
-    board(2D matrix) : board
-    row ,column : coordinates of the cell on a board
-    Returns :
-    Boolean Value
+    Esta función devuelve un valor booleano True si es seguro colocar una reina allí
+    teniendo en cuenta el estado actual de la junta.
+    Parámetros:
+    tablero (matriz 2D): tablero
+    fila, columna: coordenadas de la celda en un tablero
+    Devoluciones :
+    Valor booleano
     """
-    for i in range(len(board)):
-        if board[row][i] == 1:
+    for i in range(len(tablero)):
+        if tablero[fila][i] == 1:
             return False
-    for i in range(len(board)):
-        if board[i][column] == 1:
+    for i in range(len(tablero)):
+        if tablero[i][columna] == 1:
             return False
-    for i, j in zip(range(row, -1, -1), range(column, -1, -1)):
-        if board[i][j] == 1:
+    for i, j in zip(range(fila, -1, -1), range(columna, -1, -1)):
+        if tablero[i][j] == 1:
             return False
-    for i, j in zip(range(row, -1, -1), range(column, len(board))):
-        if board[i][j] == 1:
+    for i, j in zip(range(fila, -1, -1), range(columna, len(tablero))):
+        if tablero[i][j] == 1:
             return False
     return True
 
 
-def solve(board: list[list[int]], row: int) -> bool:
+def solve(tablero: list[list[int]], fila: int) -> bool:
     """
-    It creates a state space tree and calls the safe function until it receives a
-    False Boolean and terminates that branch and backtracks to the next
-    possible solution branch.
+    Crea un árbol de espacio de estado y llama a la función segura hasta que recibe un
+    Falso Booleano y termina esa rama y retrocede a la siguiente
+    posible rama de solución.
     """
-    if row >= len(board):
+    if fila >= len(tablero):
         """
-        If the row number exceeds N we have board with a successful combination
-        and that combination is appended to the solution list and the board is printed.
+        Si el número de fila excede N, tenemos un tablero con una combinación exitosa
+        y esa combinación se agrega a la lista de soluciones y se imprime el tablero.
         """
-        solution.append(board)
-        printboard(board)
+        solucion.append(tablero)
+        printboard(tablero)
         print()
         return True
-    for i in range(len(board)):
+    for i in range(len(tablero)):
         """
-        For every row it iterates through each column to check if it is feasible to
-        place a queen there.
-        If all the combinations for that particular branch are successful the board is
-        reinitialized for the next possible combination.
+        Para cada fila itera a través de cada columna para verificar si es factible
+        coloca una reina allí.
+        Si todas las combinaciones para esa rama en particular tienen éxito, el tablero es
+        reinicializará para la siguiente combinación posible.
         """
-        if is_safe(board, row, i):
-            board[row][i] = 1
-            solve(board, row + 1)
-            board[row][i] = 0
+        if is_safe(tablero, fila, i):
+            tablero[fila][i] = 1
+            solve(tablero, fila + 1)
+            tablero[fila][i] = 0
     return False
 
 
-def printboard(board: list[list[int]]) -> None:
+def printboard(tablero: list[list[int]]) -> None:
     """
-    Prints the boards that have a successful combination.
+    Imprime los tableros que tienen una combinación exitosa.
     """
-    for i in range(len(board)):
-        for j in range(len(board)):
-            if board[i][j] == 1:
+    for i in range(len(tablero)):
+        for j in range(len(tablero)):
+            if tablero[i][j] == 1:
                 print("Q", end=" ")
             else:
                 print(".", end=" ")
         print()
 
 
-# n=int(input("The no. of queens"))
+n=int(input("El numero de reinas"))
 n = 15
-board = [[0 for i in range(n)] for j in range(n)]
-solve(board, 0)
-print("The total no. of solutions are :", len(solution))
+tablero = [[0 for i in range(n)] for j in range(n)]
+solve(tablero, 0)
+print("El numero total de soluciones son :", len(solucion))
+
